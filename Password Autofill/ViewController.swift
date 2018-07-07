6b93ccd027beacb6d79dff57d25a7954acb14ee9
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-07-07 21:39:54 +0530

    autofill password feature

--- Password Autofill/ViewController.swift
+++ Password Autofill/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet private weak var username: UITextField!
+    @IBOutlet private weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailView" {
+            
+        }
+    }
+    
 
+    @IBAction func logInAction(_ sender: Any) {
+        
+        performSegue(withIdentifier: "detailView", sender: self)
+        
+    }
+    
+    
 }
 
