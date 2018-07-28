c7d049408c5579bc65f7303609a6761ee1fe8a92
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-07-28 22:05:56 +0530

    Passcode autofill from message app

--- Password Autofill/ViewController.swift
+++ Password Autofill/ViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+
+// If you want to use Passcode Autofill from Message app then set the content type of your password
+// textfield to One time Code. This is available from ios 12
+
 class ViewController: UIViewController {
 
     
