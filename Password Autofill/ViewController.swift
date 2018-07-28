ced05decc18332909f21d217b66a75e98ba5df83
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-07-28 22:16:10 +0530

    Strong Password suggestion

--- Password Autofill/ViewController.swift
+++ Password Autofill/ViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
+// Passcode autofill
+// If you want to use Passcode Autofill from Message app then
+// set the content type of your password textfield to One time Code.
+// This is available from ios 12
 
-// If you want to use Passcode Autofill from Message app then set the content type of your password
-// textfield to One time Code. This is available from ios 12
+// Strong Passwprd suggestion
+// To provide your user with strong password suggestion while signing up
+// you have to just set your password textfields content type to New Password
+// Again this is available from ios 12
 
 class ViewController: UIViewController {
 
